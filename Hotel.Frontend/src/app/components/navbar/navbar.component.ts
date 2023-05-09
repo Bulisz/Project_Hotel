5fd4cb7482d06b3b0732a5160ca48ae6d10ed0a3
mikig <miki.gagyi@gmail.com>
2023-05-09 15:28:11 +0200

    Make header aware of user

--- Hotel.Frontend/src/app/components/navbar/navbar.component.ts
+++ Hotel.Frontend/src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
 import { Router } from '@angular/router';
 import { AccountService } from 'src/app/services/account.service';
 import { RegistrationComponent } from '../registration/registration.component';
+import { UserModel } from 'src/app/models/user-model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  user?: UserModel | null;
 
   constructor(public dialog: MatDialog, private router: Router, private as: AccountService){  }
+  
+  ngOnInit(): void {
+    this.as.user.subscribe({
+      next: (user) => this.user = user
+    })
+  }
 
   loginPopup(){
     let dialogRef = this.dialog.open(LoginComponent)
