2950df82ef6214c1685cc371e414456846f09f8d
Bulisz <bulisz@freemail.hu>
2023-05-24 10:59:14 +0200

    EventModify and Delete backend

---
+++ Hotel.Backend/Hotel.Backend.WebAPI/Models/DTO/EventModifyDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Hotel.Backend.WebAPI.Models.DTO;
+
+public record EventModifyDTO
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public string Schedule { get; set; } = string.Empty;
+    public IFormFile? Image { get; set; }
+    public string OldImageUrl { get; set; } = string.Empty;
+}
