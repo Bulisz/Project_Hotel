5deaf7505a2a42196302c6669712f8598c9cc140
Dósa Zita <dosazita@gmail.com>
2023-05-29 10:03:38 +0200

    ts added to room-calendar

--- Hotel.Frontend/src/app/components/room-calendar/room-calendar.component.ts
+++ Hotel.Frontend/src/app/components/room-calendar/room-calendar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-room-calendar',
   templateUrl: './room-calendar.component.html',
   styleUrls: ['./room-calendar.component.css']
 })
-export class RoomCalendarComponent {
+export class RoomCalendarComponent implements OnInit {
 
+  constructor(private accountService: AccountService,
+    private router: Router,
+    public dialogRef: MatDialogRef<RoomCalendarComponent>){}
+
+
+  ngOnInit(): void {}
+
+  closeCalendar() {
+    this.dialogRef.close('ok')
+  }
 }
