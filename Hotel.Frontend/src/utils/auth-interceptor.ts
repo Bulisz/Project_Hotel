4ad84854dbed62addcf48c0e4960dcfb6853a57d
Bulisz <bulisz@freemail.hu>
2023-05-31 12:00:50 +0200

    Frontend error page handler

--- Hotel.Frontend/src/utils/auth-interceptor.ts
+++ Hotel.Frontend/src/utils/auth-interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
+import { NavigationExtras, Router } from "@angular/router";
 import { Observable, map, tap } from "rxjs";
 import { LoadingDialogComponent } from "src/app/components/loading-dialog/loading-dialog.component";
 
@@ -10,7 +11,7 @@ import { LoadingDialogComponent } from "src/app/components/loading-dialog/loadin
 
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -30,15 +31,28 @@ export class AuthInterceptor implements HttpInterceptor {
         }
       },
         (err: any) => {
+          if(err.status !== 400) {
+            const navigationExtras: NavigationExtras = {
+              state: {message: err.error, status: err.status,  statusText: err.statusText},
+            };
+            this.router.navigate(['error'], navigationExtras)
+          }
           dialogref.close()
         }));
     }
+
     return next.handle(req).pipe(tap(async (event: HttpEvent<any>) => {
       if (event instanceof HttpResponse) {
         dialogref.close()
       }
     },
       (err: any) => {
+        if(err.status !== 400) {
+          const navigationExtras: NavigationExtras = {
+            state: {message: err.error, status: err.status, statusText: err.statusText},
+          };
+          this.router.navigate(['error'], navigationExtras)
+        }
         dialogref.close()
       }));
   }
