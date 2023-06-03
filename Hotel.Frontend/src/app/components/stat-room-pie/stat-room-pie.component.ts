f7b73d88c0ef4a386e80ff907fa24e741822d944
Dósa Zita <dosazita@gmail.com>
2023-06-03 09:58:38 +0200

    pie chart added

--- Hotel.Frontend/src/app/components/stat-room-pie/stat-room-pie.component.ts
+++ Hotel.Frontend/src/app/components/stat-room-pie/stat-room-pie.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { ChartComponent } from "ng-apexcharts";
+
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
 
 @Component({
   selector: 'app-stat-room-pie',
@@ -6,5 +20,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./stat-room-pie.component.css']
 })
 export class StatRoomPieComponent {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: ChartOptions;
 
+  constructor() {
+    this.chartOptions = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        type: "donut"
+      },
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+  }
 }
