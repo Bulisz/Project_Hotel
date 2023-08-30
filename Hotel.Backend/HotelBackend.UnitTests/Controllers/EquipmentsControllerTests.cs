0cb415c7d613f197d8d61c821bc0e120ec7430df
Bulisz <bulisz@freemail.hu>
2023-08-30 16:22:43 +0200

    EquipmentRepo c Service

---
+++ Hotel.Backend/HotelBackend.UnitTests/Controllers/EquipmentsControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Hotel.Backend.WebAPI.Abstractions.Services;
+using Hotel.Backend.WebAPI.Controllers;
+using Hotel.Backend.WebAPI.Helpers;
+using Hotel.Backend.WebAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Net;
+
+namespace HotelBackend.UnitTests.Controllers;
+
+[TestClass]
+public class EquipmentsControllerTests
+{
+    private EquipmentsController _controller = null!;
+    private readonly Mock<IEquipmentService> _equipmentServiceMock = new();
+    private readonly Mock<ILogger<EquipmentsController>> _loggerMock = new();
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _controller = new EquipmentsController(_equipmentServiceMock.Object, _loggerMock.Object);
+    }
+
+    [TestMethod]
+    public async Task CreateEquipmentTest_Valid()
+    {
+        //Arrange
+        CreateEquipmentDTO createEquipmentDTO = new CreateEquipmentDTO();
+        _equipmentServiceMock.Setup(s => s.CreateEquipmentAsync(It.IsAny<CreateEquipmentDTO>())).ReturnsAsync(new EquipmentDTO() { Id = 5 });
+
+        //Act
+        var result = (await _controller.CreateEquipment(createEquipmentDTO)).Result;
+
+        //Assert
+        Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        Assert.IsInstanceOfType(((OkObjectResult)result!).Value, typeof(EquipmentDTO));
+        Assert.AreEqual(5, ((EquipmentDTO)((OkObjectResult)result!).Value!).Id);
+    }
+
+    [TestMethod]
+    public async Task CreateEquipmentTest_HotelExeption()
+    {
+        //Arrange
+        CreateEquipmentDTO createEquipmentDTO = new CreateEquipmentDTO();
+        List<HotelFieldError> errors = new() { new HotelFieldError("name", "Ilyen néven már létezik felszereltség") };
+        var ex = new HotelException(HttpStatusCode.BadRequest, errors, "One or more hotel errors occurred.");
+        _equipmentServiceMock.Setup(s => s.CreateEquipmentAsync(It.IsAny<CreateEquipmentDTO>())).Throws(ex);
+
+        //Act
+        var result = (await _controller.CreateEquipment(createEquipmentDTO)).Result;
+
+        //Assert
+        Assert.IsInstanceOfType(result, typeof(ObjectResult));
+        Assert.AreEqual(400, ((ObjectResult)result!).StatusCode);
+    }
+}
---
+++ Hotel.Backend/HotelBackend.UnitTests/Services/EquipmentServiceTests.cs
@@ -0,0 +1,123 @@
+﻿using AutoMapper;
+using Hotel.Backend.WebAPI.Abstractions.Repositories;
+using Hotel.Backend.WebAPI.Models;
+using Hotel.Backend.WebAPI.Models.DTO;
+using Hotel.Backend.WebAPI.Services;
+using Moq;
+
+namespace HotelBackend.UnitTests.Services;
+
+[TestClass]
+public class EquipmentServiceTests
+{
+    private EquipmentService _service = null!;
+    private readonly Mock<IEquipmentRepository> _equipmentReposítoryMock = new();
+    private readonly Mock<IMapper> _mapperMock = new();
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _service = new EquipmentService(_equipmentReposítoryMock.Object, _mapperMock.Object);
+    }
+
+    [TestMethod]
+    public async Task CreateEquipmentAsyncTest()
+    {
+        //Arrange
+        CreateEquipmentDTO createEquipmentDTO = new();
+        Equipment equipment = new()
+        {
+            Id = 1,
+            IsStandard = true,
+            Name = "Test",
+        };
+
+        _mapperMock.Setup(m => m.Map<Equipment>(It.IsAny<CreateEquipmentDTO>())).Returns(equipment);
+        _equipmentReposítoryMock.Setup(r => r.CreateEquipmentAsync(It.IsAny<Equipment>())).Returns<Equipment>(e => Task.FromResult(e));
+        _mapperMock.Setup(m => m.Map<EquipmentDTO>(It.IsAny<Equipment>())).Returns<Equipment>(e => new EquipmentDTO()
+        {
+            Id= e.Id,
+            Name= e.Name,
+            IsStandard = e.IsStandard,
+        });
+
+        //Act
+        var result = await _service.CreateEquipmentAsync(createEquipmentDTO);
+
+        //Assert
+        Assert.AreEqual("Test", result.Name);
+        Assert.AreEqual(true, result.IsStandard);
+        Assert.AreEqual(1, result.Id);
+    }
+
+    [TestMethod]
+    public async Task GetStandardEquipmentsTest()
+    {
+        //Arrange
+        List<Equipment> equipmentList = new()
+        {
+                new(){
+                    IsStandard = true,
+                    Name = "Test2"
+                },
+                new(){
+                    IsStandard = true,
+                    Name = "Test1"
+                },
+                new(){
+                    IsStandard = true,
+                    Name = "Test0"
+                },
+        };
+
+        _equipmentReposítoryMock.Setup(r => r.GetStandardEquipmentAsync()).ReturnsAsync(equipmentList);
+        _mapperMock.Setup(m => m.Map<List<EquipmentDTO>>(It.IsAny<List<Equipment>>())).Returns<List<Equipment>>(e => e.Select(x =>  new EquipmentDTO()
+        {
+            Id = x.Id,
+            Name = x.Name,
+            IsStandard = x.IsStandard,
+        }).ToList());
+
+        //Act
+        var result = await _service.GetStandardEquipmentsAsync();
+
+        //Assert
+        Assert.AreEqual(3, result.Count());
+    }
+
+    [TestMethod]
+    public async Task GetNonStandardEquipmentsTest()
+    {
+        //Arrange
+        List<Equipment> equipmentList = new()
+        {
+                new(){
+                    IsStandard = false,
+                    Name = "Test2"
+                },
+                new(){
+                    IsStandard = false,
+                    Name = "Test1"
+                },
+                new(){
+                    IsStandard = false,
+                    Name = "Test0"
+                },
+        };
+
+        _equipmentReposítoryMock.Setup(r => r.GetNonStandardEquipmentAsync()).ReturnsAsync(equipmentList);
+        _mapperMock.Setup(m => m.Map<List<EquipmentDTO>>(It.IsAny<List<Equipment>>())).Returns<List<Equipment>>(e => e.Select(x => new EquipmentDTO()
+        {
+            Id = x.Id,
+            Name = x.Name,
+            IsStandard = x.IsStandard,
+        }).ToList());
+
+        //Act
+        var result = await _service.GetNonStandardEquipmentsAsync();
+
+        //Assert
+        Assert.AreEqual(3, result.Count());
+    }
+
+}
