31c86e2cc08656a21be118fe5a3bda6240543031
Dósa Zita <dosazita@gmail.com>
2023-08-30 16:16:23 +0200

    GetYearStatAsync_Test done

--- Hotel.Backend/HotelBackend.UnitTests/Services/StatisticsServiceTests.cs
+++ Hotel.Backend/HotelBackend.UnitTests/Services/StatisticsServiceTests.cs
@@ -4,6 +4,7 @@ using Hotel.Backend.WebAPI.Helpers;
 using Hotel.Backend.WebAPI.Models;
 using Hotel.Backend.WebAPI.Models.DTO;
 using Hotel.Backend.WebAPI.Models.DTO.CalendarDTOs;
+using Hotel.Backend.WebAPI.Models.DTO.StatisticsDTOs;
 using Hotel.Backend.WebAPI.Services;
 using Moq;
 
@@ -82,6 +83,77 @@ namespace HotelBackend.UnitTests.Services
             Assert.IsNotNull(result);
             Assert.AreEqual(50, result[0].Percentage);
             Assert.AreEqual(10, result[1].Percentage);
+        }
+
+        [TestMethod]
+        public async Task GetYearStatAsync_Test()
+        {
+            //Arrange
+            var inputQuery = new YearStatQueryDTO 
+            {
+                Year = 2023,
+                ChoosedRooms = new List<string> {"Bodri", "Buksi"}
+            };
+
+            List<Room> choosedRooms = new List<Room>
+            {
+                new Room
+                {
+                    Id = 1,
+                    Name = "Bodri",
+                    Reservations = new List<Reservation>
+                    {
+                        new Reservation
+                        {
+                            Id = 1,
+                            BookingFrom = new DateTime (2023, 4, 1),
+                            BookingTo = new DateTime (2023, 4, 16)
+                        },
+                        new Reservation
+                        {
+                            Id = 2,
+                            BookingFrom = new DateTime (2023, 6, 1),
+                            BookingTo = new DateTime (2023, 6, 4)
+                        }
+                    }
+                },
+                new Room
+                {
+                    Id = 2,
+                    Name = "Buksi",
+                    Reservations = new List<Reservation>
+                    {
+                        new Reservation
+                        {
+                            Id = 3,
+                            BookingFrom = new DateTime (2023, 6, 1),
+                            BookingTo = new DateTime (2023, 6, 16)
+                        },
+                        new Reservation
+                        {
+                            Id = 4,
+                            BookingFrom = new DateTime (2023, 4, 1),
+                            BookingTo = new DateTime (2023, 4, 4)
+                        }
+                    }
+                }
+            };
+
+            _roomRepositoryMock.Setup(m => m.GetRoomsByNamesAsync(inputQuery.ChoosedRooms))
+           .ReturnsAsync(choosedRooms);
+
+            // Act
+            var result = (await _statisticsService.GetYearStatAsync(inputQuery)).ToList();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result[0].Data[0]);
+            Assert.AreEqual(50, result[0].Data[3]);
+            Assert.AreEqual(10, result[0].Data[5]);
+
+            Assert.AreEqual(0, result[1].Data[0]);
+            Assert.AreEqual(10, result[1].Data[3]);
+            Assert.AreEqual(50, result[1].Data[5]);
         }
     }
 }
