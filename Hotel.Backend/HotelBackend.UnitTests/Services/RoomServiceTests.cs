adeacdd8ad16efa9f672ef0d9e6ad05e57678f85
Dósa Zita <dosazita@gmail.com>
2023-08-31 13:10:34 +0200

    started

---
+++ Hotel.Backend/HotelBackend.UnitTests/Services/RoomServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using CloudinaryDotNet;
+using Hotel.Backend.WebAPI.Abstractions.Repositories;
+using Hotel.Backend.WebAPI.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBackend.UnitTests.Services
+{
+    [TestClass]
+    public class RoomServiceTests
+    {
+        private RoomService _roomService;
+        private Mock<IRoomRepository> _roomRepositoryMock;
+        private readonly Mock<IMapper> _mapperMock;
+        private Mock<Cloudinary> _cloudinaryMock;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _roomService = new RoomService
+                (
+                    _roomRepositoryMock.Object,
+                    _mapperMock.Object,
+                    _cloudinaryMock.Object
+                    
+                );
+        }
+
+        [TestMethod]
+        public async Task GetAvailableRoomsAsyncTest_withValidInqury()
+        {
+            //Arrange
+
+        }
+    }
+}
