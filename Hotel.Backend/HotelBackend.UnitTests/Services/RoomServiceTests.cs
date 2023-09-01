a575bbbb8e6603b77d2713e59fc57928b12fb000
Dósa Zita <dosazita@gmail.com>
2023-09-01 09:42:10 +0200

    RoomServiceTest added

--- Hotel.Backend/HotelBackend.UnitTests/Services/RoomServiceTests.cs
+++ Hotel.Backend/HotelBackend.UnitTests/Services/RoomServiceTests.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using CloudinaryDotNet;
 using Hotel.Backend.WebAPI.Abstractions.Repositories;
+using Hotel.Backend.WebAPI.Abstractions.Services;
+using Hotel.Backend.WebAPI.Models;
+using Hotel.Backend.WebAPI.Models.DTO;
 using Hotel.Backend.WebAPI.Services;
 using Moq;
 using System;
@@ -16,26 +19,67 @@ namespace HotelBackend.UnitTests.Services
     {
         private RoomService _roomService;
         private Mock<IRoomRepository> _roomRepositoryMock;
-        private readonly Mock<IMapper> _mapperMock;
+        private Mock<IMapper> _mapperMock;
         private Mock<Cloudinary> _cloudinaryMock;
 
         [TestInitialize]
         public void Setup()
         {
+            _roomRepositoryMock = new Mock<IRoomRepository>();
+            _mapperMock = new Mock<IMapper>();
+            _cloudinaryMock = new Mock<Cloudinary>("cloudinary://123456789012345:abcdefghijklmnopqrstuvwxyzA@cloud_name");
+
             _roomService = new RoomService
                 (
                     _roomRepositoryMock.Object,
                     _mapperMock.Object,
                     _cloudinaryMock.Object
-                    
                 );
+            
         }
 
-        [TestMethod]
-        public async Task GetAvailableRoomsAsyncTest_withValidInqury()
+        [DataTestMethod]
+        [DataRow(1, 2, 1)]
+        [DataRow(4, 5, 2)]
+        [DataRow(1, 6, 0)]
+        public async Task GetAvailableRoomsAsyncTest_withValidInqury(int choosedEq1, int choosedEq2, int expectedResult)
         {
-            //Arrange
+            // Arrange
+            var query = new RoomSelectorDTO 
+            {
+                NumberOfBeds = 1,
+                MaxNumberOfDogs = 1,
+                BookingFrom = new DateTime(2023,1,1),
+                BookingTo = new DateTime(2023,1,4),
+                ChoosedEquipments = new List<int> { choosedEq1, choosedEq2 }
+            };
+            Equipment eq1 = new Equipment { Id = 1};
+            Equipment eq2 = new Equipment { Id = 2 };
+            Equipment eq3 = new Equipment { Id = 3 };
+            Equipment eq4 = new Equipment { Id = 4 };
+            Equipment eq5 = new Equipment { Id = 5 };
+            Equipment eq6 = new Equipment { Id = 6 };
 
+            var rooms = new List<Room> 
+            {
+                new Room { Id = 1, Available = true, Equipments = new List<Equipment> {eq1, eq2, eq3 }},
+                new Room { Id = 2, Available = true, Equipments = new List<Equipment> {eq1, eq4, eq5 }},
+                new Room { Id = 3, Available = true, Equipments = new List<Equipment> {eq2, eq3, eq5 }},
+                new Room { Id = 4, Available = true, Equipments = new List<Equipment> {eq3, eq4, eq5 }}
+            };
+
+            _roomRepositoryMock.Setup(m => m.GetBigEnoughRoomsAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<List<int>>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+            .ReturnsAsync(rooms);
+            _mapperMock.Setup(m => m.Map<List<RoomListDTO>>(It.IsAny<List<Room>>())).Returns<List<Room>>(p => p.Select(x => new RoomListDTO() 
+            {
+                Id = x.Id,
+            }).ToList());
+
+            // Act
+            var result = await _roomService.GetAvailableRoomsAsync(query);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result.ToList().Count);
         }
     }
 }
