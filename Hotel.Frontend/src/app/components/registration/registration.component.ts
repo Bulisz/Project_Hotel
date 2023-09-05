69bb98cffd9c39ecda187e7cf6e61c163a51563e
Bulisz <bulisz@freemail.hu>
2023-09-05 10:10:16 +0200

    registration form fix content fix

--- Hotel.Frontend/src/app/components/registration/registration.component.ts
+++ Hotel.Frontend/src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { AccountService } from 'src/app/services/account.service';
@@ -10,24 +10,22 @@ import { validationHandler } from 'src/utils/validationHandler';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent {
 
   registerForm!: FormGroup;
 
   constructor(private accountService: AccountService,
               private router: Router,
-              public dialogRef: MatDialogRef<RegistrationComponent>){}
-
-  ngOnInit(): void {
-    this.registerForm = new FormGroup ({
-      userName: new FormControl('', [Validators.required, Validators.pattern('^(?!.*(?:admin|Admin|operator|Operator))[a-zA-Z0-9]{2,30}$')]),
-      password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*\\d)(?!.*\\s).{6,250}$')]),
-      confirmPassword: new FormControl('', [Validators.required, Validators.pattern('^(?=.*\\d)(?!.*\\s).{6,}$')]),
-      email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9.]{2,}[@][a-z0-9]{2,}[.][a-z]{2,}$')]),
-      firstName: new FormControl('', [Validators.required, Validators.pattern("^[A-ZÀ-ÖÜŐÚŰ](?!.*  )[a-zA-ZÀ-ÖØ-öø-ÿűő '-]{1,49}$")]),
-      lastName: new FormControl('', [Validators.required, Validators.pattern("^^[A-ZÀ-ÖÜŐÚŰ](?!.*  )[a-zA-ZÀ-ÖØ-öø-ÿűő '-]{1,49}$")]),
-    });
-  }
+              public dialogRef: MatDialogRef<RegistrationComponent>){
+                this.registerForm = new FormBuilder().group({
+                  userName: new FormControl('', [Validators.required, Validators.pattern('^(?!.*(?:admin|Admin|operator|Operator))[a-zA-Z0-9]{2,30}$')]),
+                  password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*\\d)(?!.*\\s).{6,250}$')]),
+                  confirmPassword: new FormControl('', [Validators.required, Validators.pattern('^(?=.*\\d)(?!.*\\s).{6,}$')]),
+                  email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9.]{2,}[@][a-z0-9]{2,}[.][a-z]{2,}$')]),
+                  firstName: new FormControl('', [Validators.required, Validators.pattern("^[A-ZÀ-ÖÜŐÚŰa-zá-öüőúű](?!.*  )[a-zA-ZÀ-ÖØ-öø-ÿűő '-]{1,49}$")]),
+                  lastName: new FormControl('', [Validators.required, Validators.pattern("^^[A-ZÀ-ÖÜŐÚŰa-zá-öüőúű](?!.*  )[a-zA-ZÀ-ÖØ-öø-ÿűő '.-]{1,49}$")])
+                });
+              }
 
   async onSubmit(){
     if(this.registerForm.valid) {
